3c8e7e2151da7d5562f53449d4c5d6b81e9b1dc5
asaduzzamanlimon420 <adilarian420@gmail.com>
2025-09-14 20:47:36 +0600

    placed git ignore

--- TalentLink/TalentLink/obj/Debug/net8.0/TalentLink.AssemblyInfo.cs
+++ TalentLink/TalentLink/obj/Debug/net8.0/TalentLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TalentLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d20f504c9ad219aed3da18d7433dc0c8d2aea9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10b4c29a03a0e0dbae9d6abb42fee08d425c026")]
 [assembly: System.Reflection.AssemblyProductAttribute("TalentLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TalentLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TalentLink/TalentLink/obj/Release/net8.0/TalentLink.AssemblyInfo.cs
+++ TalentLink/TalentLink/obj/Release/net8.0/TalentLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TalentLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a008eea345bbaa75ef8d8267651f4f22fb0dfc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10b4c29a03a0e0dbae9d6abb42fee08d425c026")]
 [assembly: System.Reflection.AssemblyProductAttribute("TalentLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TalentLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
